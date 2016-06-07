49f193ea7e70ed6b094b7bf84b17826d19362954
Andrew Greenman <agreenman@HAGERTY.COM>
2016-06-07 09:46:33 -0400

    Finished up Question 1 and implemented two algorithms for Question 2 for Chapter 2.

--- CrackTheCode/InterviewQuestions/Chapter2/TwoDotOne.cs
+++ CrackTheCode/InterviewQuestions/Chapter2/TwoDotOne.cs
@@ -1,29 +1,122 @@
 ﻿using InterviewQuestions.Infrastructure;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InterviewQuestions.Chapter2
 {
     public class TwoDotOne : Question
     {
+        private Chapter2Node _linkedList;
+
         public TwoDotOne()
-            : base(2.1, 0, 1)
+            : base(2.1, 0, 3)
         {
 
-        }
-
-        public void TwoDotOne1()
-        {
-            Chapter2Node head = LinkedListAlgorithms.CreateIntLinkedList(10, 10);
-            LinkedListAlgorithms.PrintLinkedList(head);
         }
 
         public override void Finisher(bool result, int algorithm, object[] arguments)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Algorithm {0} sorted:", algorithm);
+            LinkedListAlgorithms.PrintLinkedList(_linkedList);
+        }
+
+        public void TwoDotOne1()
+        {
+            _linkedList = LinkedListAlgorithms.CreateIntLinkedList(10, 10);
+
+            Console.WriteLine("Unsorted:");
+            LinkedListAlgorithms.PrintLinkedList(_linkedList);
+
+            HashSet<int> buffer = new HashSet<int>();
+            Chapter2Node current = _linkedList;
+            buffer.Add(current.Data);
+            while(current.Next != null)
+            {
+                if(!buffer.Contains(current.Next.Data))
+                {
+                    buffer.Add(current.Next.Data);
+                }
+                current = current.Next;
+            }
+
+            current = _linkedList;
+            bool first = true;
+            foreach(var key in buffer)
+            {
+                if(!first)
+                {
+                    current.Next = new Chapter2Node(key);
+                    current = current.Next;
+                }
+                else
+                {
+                    first = false;
+                }
+            }
+        }
+
+        // This algorithm is definitely worse in every way than algorithm 1, however the book asked how I would
+        // do this without using a buffer, so here it is in all its inefficient glory.
+        // And after looking up the solution in the book, this is the correct solution for the no buffer version.
+        // It runs in O(N^2) time with O(1) space.
+        public void TwoDotOne2()
+        {
+            _linkedList = LinkedListAlgorithms.CreateIntLinkedList(10, 10);
+
+            Console.WriteLine("Unsorted:");
+            LinkedListAlgorithms.PrintLinkedList(_linkedList);
+
+            Chapter2Node outerCurrent = _linkedList;
+            while (outerCurrent.Next != null)
+            {
+                int currentData = outerCurrent.Data;
+                Chapter2Node current = outerCurrent;
+
+                while (current.Next != null)
+                {
+                    if (current.Next.Data == currentData)
+                    {
+                        current.Next = current.Next.Next;
+                    }
+                    current = current.Next;
+
+                    if(current == null)
+                    {
+                        break;
+                    }
+                }
+
+                if(outerCurrent.Next != null)
+                {
+                    outerCurrent = outerCurrent.Next;
+                }
+            }
+        }
+
+        // Algorithm 1 requires two passes through the linked list, this algorithm cuts that down to just 1 pass.
+        public void TwoDotOne3()
+        {
+            _linkedList = LinkedListAlgorithms.CreateIntLinkedList(10, 10);
+
+            Console.WriteLine("Unsorted:");
+            LinkedListAlgorithms.PrintLinkedList(_linkedList);
+
+            HashSet<int> buffer = new HashSet<int>();
+            Chapter2Node current = _linkedList;
+            buffer.Add(current.Data);
+
+            while(current.Next != null)
+            {
+                if(buffer.Contains(current.Next.Data))
+                {
+                    current.Next = current.Next.Next;
+                }
+                else
+                {
+                    buffer.Add(current.Next.Data);
+                    current = current.Next;
+                }
+            }
         }
     }
 }
---
+++ CrackTheCode/InterviewQuestions/Chapter2/TwoDotTwo.cs
@@ -0,0 +1,87 @@
+﻿using InterviewQuestions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions.Chapter2
+{
+    public class TwoDotTwo : Question
+    {
+        private struct Bucket
+        {
+            public int K, Value;
+            public Bucket(int k , int value)
+            {
+                K = k;
+                Value = value;
+            }
+        }
+
+        public TwoDotTwo()
+            : base(2.2, 1, 2)
+        {
+
+        }
+
+        public override void Finisher(bool result, int algorithm, object[] arguments)
+        {
+            // Do nothing.
+        }
+        
+        // This algorithm goes through the whole linked list to get a size, then goes through it again until
+        // it finds the element at position {size - k}. This algorithm at takes O(N) time.
+        public void TwoDotTwo1(string value)
+        {
+            int k = Convert.ToInt32(value);
+            Chapter2Node head = LinkedListAlgorithms.CreateIntLinkedList(100, 10);
+            LinkedListAlgorithms.PrintLinkedList(head);
+
+            int count = 1;
+            Chapter2Node current = head;
+            while(current.Next != null)
+            {
+                current = current.Next;
+                count++;
+            }
+
+            current = head;
+            while(count > k)
+            {
+                current = current.Next;
+                count--;
+            }
+
+            Console.WriteLine("{0} elements from the end: {1}", k, current.Data);
+        }
+
+        // This algorithm does something similar to the first, except it counts k elements from the end.
+        // This is a recursive solution and takes O(N) time.
+        public void TwoDotTwo2(string value)
+        {
+            int k = Convert.ToInt32(value);
+            Chapter2Node head = LinkedListAlgorithms.CreateIntLinkedList(20, 20);
+            LinkedListAlgorithms.PrintLinkedList(head);
+
+            Console.WriteLine("{0} elements from the end: {1}", k, TwoDotTwoRecursive(new Bucket(k, 0), head).Value);
+        }
+
+        private Bucket TwoDotTwoRecursive(Bucket b, Chapter2Node current)
+        {
+            if (current.Next != null)
+            {
+                Bucket check = TwoDotTwoRecursive(new Bucket(b.K, b.Value), current.Next);
+
+                if (check.K != 0)
+                {
+                    return new Bucket(--check.K, current.Data);
+                }
+
+                return check;
+            }
+
+            return new Bucket(--b.K, current.Data);
+        }
+    }
+}
